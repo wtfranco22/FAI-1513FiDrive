33a3f2c716a0f2871fd1b68c78183a3a00a4f9ae
wtfranco22 <francofranco_96@hotmail.com>
2020-09-28 19:28:17 -0300

    Agregue los nuevos estilos al TPEntregable

---
+++ vista/TPE/1/accion1.php
@@ -0,0 +1,18 @@
+
+<?php
+include_once("../../estructura/cabecera.php");
+?>
+<div id="accion1" name="accion1" class="col-md-9 col-lg-10 bg-light">
+    <?php
+    $datos = data_submitted();
+    $obj = new control();
+    $respuesta= $obj->prueba();
+    echo "<h2>".$respuesta."</h2>";
+    ?>
+    <br/>
+    <a class="btn btn-primary" href='index1.php'>volver</a>
+</div>
+
+<?php
+include_once("../../estructura/pie.php");
+?>
---
+++ vista/TPE/1/index1.php
@@ -0,0 +1,16 @@
+<?php
+include_once("../../estructura/cabecera.php");
+?>
+
+<div class="p-3 col-md-9 col-lg-10 bg-white">
+    <form id="ejemplo1" name="ejemplo1" action="accion1.php" method="POST" data-toggle="validator">
+        <h2>
+            Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.Mi nombre es Franco matias rodriguez y mi Nro de Legajo FAI-1513
+        </h2>
+        <button type="submit" class="btn btn-success" >Probar</button>
+    </form>
+</div>
+
+<?php
+include_once("../../estructura/pie.php");
+?>
