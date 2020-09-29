a6bdab7c2f1e0f6b660a36fa9bfe99602c5f0b3a
wtfranco22 <francofranco_96@hotmail.com>
2020-09-29 04:08:35 -0300

    Se agregan nuevos direccionamientos para los ejercicios agregados

--- vista/estructura/lateral.php
+++ vista/estructura/lateral.php
@@ -1,6 +1,6 @@
 <div class="row shadow">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
-    <button class="btn btn-dark shadow mt-5 mb-5 shadow" data-toggle="collapse" data-target="#tp1">Trabajo Practico 1</button>
+        <button class="btn btn-dark shadow mt-5 mb-5 shadow" data-toggle="collapse" data-target="#tp1">Trabajo Practico 1</button>
         <div id="tp1" class="collapse">
             <div class="list-group">
                 <a href="../../TP1/ejercicio_1/index1.php" class="list-group-item">
@@ -24,11 +24,25 @@
         <button class="btn btn-dark shadow mt-5 mb-5 shadow" data-toggle="collapse" data-target="#tp2">Trabajo Practico 2</button>
         <div id="tp2" class="collapse">
             <div class="list-group">
-                <a href="../../TP2/ejercicio_3/index11.php" class="list-group-item">
+                <a href="../../TP2/ejercicio_3/index3.php" class="list-group-item">
                     Ejercicio 1</a>
-                <a href="../../TP2/ejercicio_4/index12.php" class="list-group-item">
+                <a href="../../TP2/ejercicio_4/index4.php" class="list-group-item">
                     Ejercicio 2</a>
             </div>
         </div>
+        <button class="btn btn-dark shadow mt-5 mb-5 shadow" data-toggle="collapse" data-target="#tpe">Trabajos Practicos E</button>
+        <div id="tpe" class="collapse">
+            <div class="list-group">
+                <a href="../../TPE/1/index1.php" class="list-group-item">
+                    Entrega 1</a>
+                <a href="../../TPE/2/armarchivo.php" class="list-group-item">
+                    Armar archivo</a>
+                <a href="../../TPE/2/compartirarchivo.php" class="list-group-item">
+                    Compartir archivo</a>
+                <a href="../../TPE/2/eliminararchivocompartido.php" class="list-group-item">
+                    Eliminar archivo compartido</a>
+                <a href="../../TPE/2/eliminararchivo.php" class="list-group-item">
+                    Eliminar archivo</a>
+            </div>
+        </div>
     </nav>
-    
