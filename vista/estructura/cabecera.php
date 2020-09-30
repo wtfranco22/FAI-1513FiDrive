e3e4afb917feaf55df29f8bf8c6735e4116b8b22
wtfranco22 <francofranco_96@hotmail.com>
2020-09-30 00:31:07 -0300

    solo cambie color icon

--- vista/estructura/cabecera.php
+++ vista/estructura/cabecera.php
@@ -11,10 +11,10 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" />
     <style>
         /*Como son unas lineas de codigo, los deje para variar un poco los iconos*/
-        .fa-smile {
+        .fa-thumbs-up{
             color: green;
         }
-        .fa-angry {
+        .fa-thumbs-down{
             color: red;
         }
         .placeicon {
